379c9ffc848ac1144705ca80728e8cb738b2a32d
GuTing <goodguting@gmail.com>
2021-01-21 17:38:56 +0800

    增加config-client

---
+++ gt-config-client/src/main/java/com/gt/config/client/controller/ConfigClientController.java
@@ -0,0 +1,17 @@
+package com.gt.config.client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigClientController {
+
+    @Value("${foo}")
+    String foo;
+
+    @GetMapping(value = "/hi")
+    public String hi() {
+        return foo;
+    }
+}

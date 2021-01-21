f3d2336e5302619fef8e40a160d63490a96573cf
GuTing <goodguting@gmail.com>
2021-01-21 17:59:53 +0800

    no message

--- gt-config-client/src/main/java/com/gt/config/client/ConfigClientApplication.java
+++ gt-config-client/src/main/java/com/gt/config/client/ConfigClientApplication.java
@@ -1,12 +1,11 @@
 package com.gt.config.client;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ConfigClientApplication {
 
     public static void main(String[] args) {

150a86d80bf34312bf981667acbdd2f2f1515dbd
GuTing <goodguting@gmail.com>
2021-01-28 14:36:59 +0800

    增加hystrix dashbord

--- gt-feign/src/main/java/com/gt/feign/FeignApplication.java
+++ gt-feign/src/main/java/com/gt/feign/FeignApplication.java
@@ -2,12 +2,14 @@ package com.gt.feign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients //允许服务调用
 @EnableEurekaClient//允许注册到注册中心
+@EnableCircuitBreaker
 public class FeignApplication {
 
     public static void main(String[] args) {

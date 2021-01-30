af1953357e9ece9315223acd7005040a254b867f
GuTing <goodguting@gmail.com>
2021-01-30 16:59:07 +0800

    增加gateway

---
+++ gt-gateway/src/main/java/com/gt/gateway/GatewayApplication.java
@@ -0,0 +1,14 @@
+package com.gt.gateway;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}

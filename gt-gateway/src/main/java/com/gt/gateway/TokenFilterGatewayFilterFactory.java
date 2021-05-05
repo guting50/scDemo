ae7cde9b18bb5d1758b35642d338ee4976e89f40
GuTing <goodguting@gmail.com>
2021-05-05 17:03:57 +0800

    no message

--- gt-gateway/src/main/java/com/gt/gateway/TokenFilterGatewayFilterFactory.java
+++ gt-gateway/src/main/java/com/gt/gateway/TokenFilterGatewayFilterFactory.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
 import java.util.List;
